08ca2277873a0307ae95d1f16500cb804ba06157
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2021-03-16 10:30:23 +0800

    added webpack for dev and prod bundling

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // absolute path of the ouput dir
+    publicPath: 'dist' // inform where is the ouput located.
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin'); //clear the dist
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // absolute path of the ouput dir
+  },
+  devtool: 'nosources-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin()
+  ]
+};
